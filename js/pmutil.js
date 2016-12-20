47b6862fe8371cd50721f0dc193305b7fce19450
mortie <martid0311@gmail.com>
2016-12-20 16:46:53 +0100

    process management works better now

--- js/pmutil.js
+++ js/pmutil.js
@@ -13,63 +13,75 @@ var restartLimit = 10;
 class Process {
 	constructor(id, cmd, options) {
 		this.proc = null;
-		this.running = false;
-		this.stopped = true;
 		this.id = id;
 		this.cmd = cmd;
 		this.options = options;
+		this.state = "stopped";
+		this.running = false;
 
-		this.totalRestarts = 0;
 		this.restarts = 0;
 		this.restartsResetTimeout = null;
-		this.restartLimit = 5;
+		this.restartLimit = 15;
 
 		console.log("Created process "+this.id);
 	}
 
 	onexit(code) {
+
+		this.log("Process exited.");
 		this.running = false;
 
-		// If the process exits, but we haven't stopped it, try to restart it
-		if (!this.stopped) {
+		// If the process isn't supposed to be running,
+		// just don't do anything
+		if (this.state !== "running")
+			return;
 
-			if (this.restarts >= this.restartLimit) {
-				this.formatMsg("Not restarting anymore after "+this.restarts+" restarts.");
-			} else {
-				this.formatMsg("Restarting after exit with code "+code);
-				this.restarts += 1;
-				this.totalRestarts += 1;
+		this.state = "errored";
+
+		if (this.restarts >= this.restartLimit) {
+			this.log("Not restarting anymore after "+this.restarts+" restarts.");
+			this.stop(function() {});
+		} else {
+			this.restarts += 1;
+
+			this.log(
+				"Restarting in "+this.restarts+" seconds "+
+					"after exit with code "+code+".");
+
+			if (this.restartsResetTimeout) {
+				clearTimeout(this.restartsResetTimeout);
+				this.restartsResetTimeout = null;
+			}
+
+			setTimeout(() => {
 				this.start();
-
-				if (this.restartsResetTimeout) {
-					clearTimeout(this.restartsResetTimeout);
-					this.restartsResetTimeout = null;
-				}
 				this.restartsResetTimeout = setTimeout(() => {
 					this.restarts = 0;
 				}, 5000);
-			}
+			}, this.restarts * 1000);
 		}
 	}
 
 	start() {
-		if (this.running && !this.stopped)
+		if (this.state === "running")
 			throw "Process "+this.id+" already running.";
 
-		this.stopped = false;
+		this.log("Started process.");
+
 		this.running = true;
+		this.state = "running";
 		this.proc = childProcess.exec(this.cmd, this.options);
 
 		this.proc.stdout.on("data", d => {
 			d.toString().split("\n").forEach(l => {
 				if (l.trim() === "") return;
-				this.formatMsg(l);
+				this.log("stdout: "+l);
 			});
 		});
 		this.proc.stderr.on("data", d => {
 			d.toString().split("\n").forEach(l => {
 				if (l.trim() === "") return;
-				this.formatMsg("stderr: "+l);
+				this.log("stderr: "+l);
 			});
 		});
 
@@ -77,46 +89,42 @@ class Process {
 	}
 
 	stop(cb) {
-		this.stopped = true;
+		this.state = "stopped";
 		this.proc.kill("SIGTERM");
 		var cbd = false;
 
 		// SIGKILL if we haven't exited in 2 seconds
 		setTimeout(() => {
-			if (this.stopped && this.running) {
-				console.log("Killing process "+this.id+" because it didn't stop on SIGTERM.");
+			if (this.state === "stopped" && this.running) {
+				this.log("Killing process because it didn't stop on SIGTERM.");
 				this.proc.kill("SIGKILL");
 
 				if (!cbd) {
-					cb();
 					cbd = true;
+
+					setTimeout(cb, 100);
 				}
 			}
 		}, 1000);
 
 		setTimeout(() => {
 			if (!this.running && !cbd) {
-				cb();
 				cbd = true;
+				setTimeout(cb, 100);
 			}
 		}, 0);
 	}
 
-	formatMsg(msg) {
-		console.log("process "+this.id+": "+msg);
+	log(msg) {
+		var str = "Process '"+this.id+"': "+msg;
+		console.log(str);
 	}
 
 	serialize() {
-		var state;
-		if (this.stopped && !this.running) state = "stopped";
-		else if (!this.stopped && this.running) state = "running";
-		else if (!this.stopped && !this.running) state = "errored";
-		else state = "invalid";
-
 		return {
 			id: this.id,
-			state: state,
-			restarts: this.totalRestarts
+			state: this.state,
+			restarts: this.restarts
 		};
 	}
 }
