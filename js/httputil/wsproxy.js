6ca6a0038d69913610cd002166b2c3e4c18ade4b
mortie <martid0311@gmail.com>
2017-06-11 13:15:29 +0200

    fixed websocket proxies

--- js/httputil/wsproxy.js
+++ js/httputil/wsproxy.js
@@ -1,6 +1,15 @@
 var WSServer = require("ws").Server;
 var WSClient = require("ws");
 
+// Some codes are reserved, so we need to translate them
+// into something else
+function translateCode(code) {
+	if (code === 1004 || code === 1005 || code === 1006)
+		return 1000;
+	else
+		return code;
+}
+
 function onWSSocket(wsdomains, sock) {
 	var req = sock.upgradeReq;
 	if (typeof req.headers.host !== "string") {
@@ -14,59 +23,47 @@ function onWSSocket(wsdomains, sock) {
 	if (action === undefined)
 		return console.log("Unknown websocket host: "+domain);
 
-	var sockQueue = [];
 	var psockQueue = [];
-	var sockOpened = false;
-	var psockOpened = false;
-	var sockClosed = false;
 	var psockClosed = false;
-
+	var sockClosed = false;
 	var psock = new WSClient(action.websocket);
 
 	// Send messages, or queue them up
 	sock.on("message", msg => {
 		if (psock.readyState === 1) {
-			if (!psockOpened) {
-				psockQueue.forEach(m => psock.send(m));
-				psockQueue = null;
-				psockOpened = true;
-			}
 			psock.send(msg);
-		} else if (!psockOpened) {
+		} else {
 			psockQueue.push(msg);
 		}
 	});
+	psock.on("open", () => {
+		psockQueue.forEach(msg => psock.send(msg));
+		psockQueue = null;
+	});
+
 	psock.on("message", msg => {
-		if (sock.readyState === 1) {
-			if (!sockOpened) {
-				sockQueue.forEach(m => sock.send(m));
-				sockQuee = null;
-				sockOpened = true;
-			}
-			sock.send(msg);
-		} else if (!sockOpened) {
-			sockQueue.push(msg);
-		}
+		sock.send(msg);
 	});
 
 	// Close one socket when the other one closes
 	sock.on("close", (code, msg) => {
 		sockClosed = true;
 		if (!psockClosed)
-			psock.close(code, msg);
+			psock.close(translateCode(code), msg);
 	});
 	psock.on("close", (code, msg) => {
 		psockClosed = true;
-		if (!psockClosed)
-			sock.close(code, msg);
+		if (!sockClosed)
+			sock.close(translateCode(code), msg);
 	});
 
 	// Catch errors
 	sock.on("error", err => {
-		console.trace(err);
+		console.log("websocket proxy:", err.code);
 	});
 	psock.on("error", err => {
-		console.trace(err);
+		if (err.code !== "ECONNRESET")
+			console.log("websocket proxy:", err.code);
 	});
 }
 
